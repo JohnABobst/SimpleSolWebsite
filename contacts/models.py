8e367f6f32732a0aaf9386e6da75bb0aba518b43
John Bobst <johnabobst@gmail.com>
2018-02-21 17:00:08 -0800

    Fixed contacts
    Fixed default settings for contacts

--- contacts/models.py
+++ contacts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Contacts(models.Model):
-    contact_name = models.CharField(max_length=256, default='rabble')
+    contact_name = models.CharField(max_length=256, default='contact name')
     phone_number = models.CharField(max_length=15)
-    phone_ext = models.CharField(max_length=10, default='rabble', blank="True")
+    phone_ext = models.CharField(max_length=10, default='ext', blank="True")
     email = models.EmailField()
     comments = models.TextField()
-    company_name = models.CharField(max_length=250, default='rabble')
+    company_name = models.CharField(max_length=250, default='company name')
