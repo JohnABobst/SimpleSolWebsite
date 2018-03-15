bd7d9de7a974e418057c14cc94355031579a8e06
John Bobst <johnabobst@gmail.com>
2018-03-15 13:15:08 -0700

    Django send mail
    Used built in Django function for sending email so that gunicorn doesn't time out.

--- contacts/views.py
+++ contacts/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from contacts.forms import ContactForm
 from django.views.generic import TemplateView
-from exchangelib import *
-from exchangelib.items import *
+from django.core.mail import send_mail
+from django.conf import settings
 
 class ContactRequestView(FormView):
     template_name = 'contact_form.html'
@@ -11,29 +11,20 @@ class ContactRequestView(FormView):
     success_url = '/contact/thanks/'
 
 
+
+
     def form_valid(self, form):
-        credentials = Credentials(
-            username='simplesol.com\\jbobst',
-            password='Pa55word!'
-        )
-        account = Account(
-            primary_smtp_address='jbobst@simplesol.com',
-            credentials=credentials,
-            autodiscover=True,
-            access_type=DELEGATE
-            )
-        audit_request = ''
+        subject = ''
         for k in form.cleaned_data:
-            audit_request += k + ' = ' + form.cleaned_data[k] + '\n'
+            subject += k.replace("_", " ") + ' = ' + form.cleaned_data[k] + '\n'
+        send_mail(
+        "You have a new contact request",
+        subject,
+        settings.EMAIL_HOST_USER,
+        ['scheduling@simplesol.com', 'sales@simplesol.com'],
+        fail_silently = False
 
-        m = Message(
-           account=account,
-           subject='You have a new site audit request',
-            body= audit_request,
-            to_recipients=['sales@simplesol.com','scheduling@simplesol.com'],
-            )
-
-        m.send_and_save()
+        )
 
         form.save()
         return super().form_valid(form)
--- site_audit/views.py
+++ site_audit/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from site_audit.forms import AuditRequestForm
 from django.views.generic import TemplateView
-from django.core.mail import send_mail, get_connection
-from exchangelib import *
-from exchangelib.items import *
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # Create your views here.
@@ -17,28 +16,17 @@ class AuditRequestView(FormView):
 
 
     def form_valid(self, form):
-        credentials = Credentials(
-            username='simplesol.com\\jbobst',
-            password='Pa55word!'
-        )
-        account = Account(
-            primary_smtp_address='jbobst@simplesol.com',
-            credentials=credentials,
-            autodiscover=True,
-            access_type=DELEGATE
-        )
-        audit_request = ''
+        subject = ''
         for k in form.cleaned_data:
-            audit_request += k + ' = ' + form.cleaned_data[k] + '\n'
+            subject += k.replace("_", " ") + ' = ' + form.cleaned_data[k] + '\n'
+        send_mail(
+        "You have a new site audit request",
+        subject,
+        settings.EMAIL_HOST_USER,
+        ['scheduling@simplesol.com', 'sales@simplesol.com'],
+        fail_silently = False
 
-        m = Message(
-           account=account,
-           subject='You have a new site audit request',
-            body= audit_request,
-            to_recipients=['sales@simplesol.com', 'scheduling@simplesol.com'],
-            )
-
-        m.send_and_save()
+        )
 
         form.save()
         return super().form_valid(form)
