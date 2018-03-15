74a16923b7e6335d46ba346c007cd466cbc9b0ec
RabbleMonster <johnabobst@gmail.com>
2018-03-15 12:13:28 -0700

    removed code that sends emails because it was timing out system

--- contacts/views.py
+++ contacts/views.py
@@ -12,30 +12,7 @@ class ContactRequestView(FormView):
 
 
     def form_valid(self, form):
-        credentials = Credentials(
-            username='simplesol.com\\jbobst',
-            password='Pa55word!'
-        )
-        account = Account(
-            primary_smtp_address='jbobst@simplesol.com',
-            credentials=credentials,
-            autodiscover=True,
-            access_type=DELEGATE
-            )
-        audit_request = ''
-        for k in form.cleaned_data:
-            audit_request += k + ' = ' + form.cleaned_data[k] + '\n'
-
-        m = Message(
-           account=account,
-           subject='You have a new site audit request',
-            body= audit_request,
-            to_recipients=['sales@simplesol.com','scheduling@simplesol.com'],
-            )
-
-        m.send_and_save()
-
-        form.save()
+        form.save() 
         return super().form_valid(form)
 
 
